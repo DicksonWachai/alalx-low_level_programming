9214ad22a44e95870c8c3662bccf9f29cf42a4c1
DicksonWachai <wachaidickson@gmail.com>
2023-11-07 01:22:06 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
